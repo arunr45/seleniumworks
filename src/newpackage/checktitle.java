711a1a07701b8fd42403a611783475fc4b63beb4
arunr45 <arunpanicker45@gmail.com>
2021-04-22 19:42:43 +0530

    Testing the page title

---
+++ src/newpackage/checktitle.java
@@ -0,0 +1,30 @@
+package newpackage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class checktitle {
+	
+	public static void main(String[] args) throws InterruptedException {
+	
+	String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String expectedTitle = "Welcome: Mercury Tours";
+    String actualTitle = "";
+	
+    System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get(baseUrl);
+	actualTitle=driver.getTitle();
+	driver.manage().window().maximize();
+	driver.manage().window().wait();
+	if(actualTitle.contentEquals(expectedTitle))
+    
+	{
+		System.out.println("Matching");
+	}
+	else
+	{
+	System.out.println("Not matching");
+	}
+	
+driver.close();
+}}
